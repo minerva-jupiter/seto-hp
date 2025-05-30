007ce6a42260b4e3b7df3a4c45fcf6099b3ff51d
minerva-jupiter <ryouturn@gmail.com>
2025-05-30 23:19:19 +0900

    delete matryoshika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,36 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="jp">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
+  return ( <>
+  <html lang="jp" className="h-100">
+    <body>
+      <header>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">瀬戸和希のホームページ</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="https://www.staffsearch.shimane-u.ac.jp/kenkyu/search/3d57492f13a5ff11d57e2a307cd68dcd/detail">島根大学教員情報検索</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main >
+        <div className="container">
+          {children}
+        </div>
+      </main>
+    </body>
+  </html>
+  </>
   );
 }
